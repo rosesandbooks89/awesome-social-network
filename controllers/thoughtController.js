da3578ea3fc5dea30d3a8b4f8777217c73b5616f
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-09-08 20:43:46 -0500

    finished thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -39,10 +39,61 @@ async getThoughtById({ params }, res) {
     }
 },
 
-
-
-// create thought
 // update thought by id
+async updateThought({ params, body }, res) {
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(params.id, body, { new: true });
+      if (!updatedThought) {
+        return res.status(404).json({ message: 'No thought found with this ID!' });
+      }
+      res.json(updatedThought);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+},
+
 // delete thought by id
+ async deleteThought({ params }, res) {
+    try {
+      const thought = await Thought.findByIdAndRemove(params.id);
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought found with this ID!' });
+      }
+      // remove from user's thoughts array
+      await User.findByIdAndUpdate(thought.userId, { $pull: { thoughts: thought._id } });
+      res.json({ message: 'Thought deleted successfully!' });
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+},
 // add reaction to thought
+
+async addReaction({ params, body }, res) {
+    try {
+        console.log("reaction:", params.thoughtID);
+    const updatedThought = await Thought.findByIdAndUpdate(params.thoughtId, { $push: { reactions: body } }, { new: true });
+    if (!updatedThought) {
+        return res.status(404).json({ message: 'No thought found with this ID!' });
+    }
+    res.json(updatedThought);
+    } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+    }
+},
 // delete reaction from thought
+async deleteReaction({ params }, res) {
+    try {
+      const updatedThought = await Thought.findByIdAndUpdate(params.thoughtId, { $pull: { reactions: { reactionId: params.reactionId } } }, { new: true });
+      if (!updatedThought) {
+        return res.status(404).json({ message: 'No thought found with this ID!' });
+      }
+      res.json(updatedThought);
+    } catch (err) {
+      console.error(err);
+      res.status(400).json(err);
+    }
+  }
+};
